e7d9bb333341efcf0f167fb27857693b55954ef8
hjs90911 <hjs90911@naver.com>
2018-06-22 17:24:11 +0900

    Update harvest_honey.c

--- etc/SWEA_2115_HarvestHoney/harvest_honey.c
+++ etc/SWEA_2115_HarvestHoney/harvest_honey.c
@@ -1,46 +1,58 @@
 #define _CRT_SECURE_NO_WARNINGS
-typedef enum {false, true} bool;
 #include <stdio.h>
 #include <stdlib.h>
 
 int** hive;		// map of hive
-bool** empty;	// check if empty
 int N;			// size of hive
 int M;			// selective hive
 int C;			// maximum honey amount
 int hy, hx;		// harvested coordination
 int hcnt;		// harvested count
-int max_profit;	// maximum profit
+int max_profit;		// maximum profit
 
-void harvest(int y, int x, int cnt, int total, int profit)
+void harvest()
 {
-	if (y == N - 1 && x == N - 1)
-		return;
-	else if (x == N - 1)
+	for (int i = 0; i < N; i++)
 	{
-		if (max_profit < profit)
+		for (int j = 0; j < N; j++)
 		{
-			hy = y;
-			hx = x;
-			hcnt = cnt;
-			max_profit = profit;
+			int cnt = 0;
+			int total = 0;
+			int profit = 0;
+			int k = j;
+
+			while (1)
+			{
+				if (hive[i][k] == -1 ||
+					total + hive[i][k] > C ||
+					cnt == M)
+					break;
+				else if (k == N - 1)
+				{
+					total += hive[i][k];
+					profit += hive[i][k] * hive[i][k];
+					cnt++;
+					break;
+				}
+				else
+				{
+					total += hive[i][k];
+					profit += hive[i][k] * hive[i][k];
+				}
+
+				cnt++;
+				k++;
+			}
+
+			if (max_profit < profit)
+			{
+				hy = i;
+				hx = k;
+				hcnt = cnt;
+				max_profit = profit;
+			}
 		}
-
 	}
-	else
-	{
-		// normal case
-		int nx = x + 1;
-		int nTotal = total + hive[y][nx];
-		int nProfit = profit + hive[y][nx] * hive[y][nx];
-
-		if (nTotal > C)
-		{
-
-		}
-		harvest(y, nx, cnt + 1, nTotal, nProfit);
-	}
-
 }
 
 void input()
@@ -61,31 +73,35 @@ int main()
 	scanf("%d", &T);
 
 	//for (int test_case = 1; test_case <= T; test_case++)
-	for (int test_case = 1; test_case <= 1; test_case++)
+	for (int test_case = 1; test_case <= 3; test_case++)
 	{
 		max_profit = 0;
 		scanf("%d %d %d", &N, &M, &C);
 
 		// malloc
 		hive = (int**)malloc(sizeof(int*) * N);
-		empty = (bool**)malloc(sizeof(bool*) * N);
 		for (int i = 0; i < N; i++)
 		{
 			hive[i] = (int*)malloc(sizeof(int) * N);
-			empty[i] = (bool*)calloc(N, sizeof(bool) * N);
 		}
 
 		input();
 
+		harvest();
+		int temp = max_profit;
+		max_profit = 0;
+		printf("%d %d %d\n", hy, hx, hcnt);
+		for (int i = 0; i < hcnt; i++)
+			hive[hy][hx - i] = -1;
+		harvest();
+		printf("%d %d %d\n", hy, hx, hcnt);
+
+		printf("#%d %d\n", test_case, temp + max_profit);
 
 		// free
 		for (int i = 0; i < N; i++)
-		{
 			free(hive[i]);
-			free(empty[i]);
-		}
 		free(hive);
-		free(empty);
 	}
 	
 	return 0;
