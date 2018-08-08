beff1c70f3bc23131cc1ad0c91fb92d8ae1236c9
hjs90911 <hjs90911@naver.com>
2018-08-08 22:48:47 +0900

    Update microbe_isolation.cpp

--- ongoing/SWEA_2382_MicrobeIsolation/microbe_isolation.cpp
+++ ongoing/SWEA_2382_MicrobeIsolation/microbe_isolation.cpp
@@ -1,6 +1,12 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
+
+const int UP    = 1;
+const int DOWN  = 2;
+const int LEFT  = 3;
+const int RIGHT = 4;
 
 class Microbe
 {
@@ -14,10 +20,61 @@ public:
 	Microbe(int _y, int _x, int _num, int _dir) : y(_y), x(_x), num(_num), dir(_dir) {}
 };
 
-Microbe gp[1000];		// array of microbe
+vector<Microbe> gp;		// array of microbe
 int N;					// number of cell
 int M;					// isolation time
 int K;					// number of group
+
+bool isOnEdge(Microbe& m)
+{
+	if (m.y == 0 || m.y == N - 1 || m.x == 0 || m.x == N - 1)
+	{
+		m.num /= 2;
+		return true;
+	}
+	return false;
+}
+
+void move(Microbe& m)
+{
+	if (isOnEdge(m))
+	{
+		switch (m.dir)
+		{
+		case UP:
+			m.dir = DOWN;
+			break;
+		case DOWN:
+			m.dir = UP;
+			break;
+		case LEFT:
+			m.dir = RIGHT;
+			break;
+		case RIGHT:
+			m.dir = LEFT;
+			break;
+		}
+	}
+	else
+	{
+		switch (m.dir)
+		{
+		case UP:
+			m.y -= 1;
+			break;
+		case DOWN:
+			m.y += 1;
+			break;
+		case LEFT:
+			m.x -= 1;
+			break;
+		case RIGHT:
+			m.x += 1;
+			break;
+		}
+	}
+
+}
 
 int main()
 {
