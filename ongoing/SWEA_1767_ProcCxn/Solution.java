fefd502861814cd50c342011c260c2c9835c7e2f
hjs90911 <hjs90911@naver.com>
2018-09-21 16:45:38 +0900

    Update Solution.java

--- ongoing/SWEA_1767_ProcCxn/Solution.java
+++ ongoing/SWEA_1767_ProcCxn/Solution.java
@@ -1,9 +1,13 @@
 import java.io.FileInputStream;
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
 
 	// LEFT, UP, RIGHT, DOWN
+	final int LEFT  = 0;
+	final int UP    = 1;
+	final int RIGHT = 2;
+	final int DOWN  = 3;
 	final int[] dy  = {0, -1, 0, 1};
 	final int[] dx  = {-1, 0, 1, 0};
 	
@@ -19,69 +23,63 @@ public class Solution {
 	}
 	
 	Scanner sc = new Scanner(System.in);
-	Coord[] core = new Coord[12];						// array of core coordination
-	ArrayList<Integer> seq;							// array of sequence
-	int[][] cell;								// array of cell
-	int N;									// size of cell	
-	int core_cnt;								// count of core
-	int len_sum;								// length sum
-	int max_cnt;								// maximum count of core
-	int min_len;								// minimum length of line
+	Coord[] core = new Coord[12];					// array of core coordination
+	boolean[] visited = new boolean[12];				// check if visited
+	int[][] cell;							// array of cell
+	int N;								// size of cell	
+	int core_cnt;							// count of core
+	int max_cnt;							// maximum count of core
+	int min_len;							// minimum length of line
 	
 	
-	void connectLine() {
-		int pcell[][] = new int[N][N];
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				pcell[i][j] = cell[i][j];
-			}
-		}
-		
-		int sum = 0, cnt = 0;
-		
-		for (int i = 0; i < core_cnt; i++) {
-			int y = core[i].y, x = core[i].x;
+	boolean isConflicted(int y, int x, int dir) {
+		while (y != 0 && y != N - 1 && x != 0 && x != N - 1) {
+			y += dy[dir];
+			x += dx[dir];
 			
-			while (true) {
-				y += dy[seq.get(i)];
-				x += dx[seq.get(i)];
-				if (y < 0 || y >= N || x < 0 || x >= N) {
-					cnt += 1;
-					break;
-				}
-				
-				if (pcell[y][x] == 1)
-					return;
-				else
-					pcell[y][x] = 1;
-				sum++;
-			}
+			if (cell[y][x] != 0)
+				return true;
 		}
-		if (max_cnt < cnt) {
-			max_cnt = cnt;
-			min_len = sum;
-		}
-		else if (max_cnt == cnt && min_len > sum)
-			min_len = sum;
+		return false;
 	}
 	
-	void dfs(int n) {
-		if (n == core_cnt) {
-			connectLine();
-			return;
+	int sumLineLength(int y, int x, int dir, boolean connected) {
+		int len_sum = 0;
+		while (y != 0 && y != N - 1 && x != 0 && x != N - 1) {
+			y += dy[dir];
+			x += dx[dir];
+			len_sum++;
+			
+			cell[y][x] = connected ? 0 : 2;
 		}
-		
-		for (int i = 0; i < 4; i++) {
-			seq.add(i);
-			dfs(n + 1);
-			seq.remove(seq.size() - 1);
+		return len_sum;
+	}
+	
+	void findMinLen(int core_idx, int core_num, int len) {
+		if (core_idx == core_cnt) {
+			if (max_cnt < core_num) {
+				max_cnt = core_num;
+				min_len = len;
+			} else if (max_cnt == core_cnt) {
+				min_len = min_len > len ? len : min_len;
+			}
+		} else {
+			for (int i = 0; i < 4; i++) {
+				int y = core[core_idx].y;
+				int x = core[core_idx].x;
+				if (!isConflicted(y, x, i)) {
+					findMinLen(core_idx + 1, core_num + 1, len + sumLineLength(y, x, i, false));
+					sumLineLength(y, x, i, true);
+				}
+			}
+			findMinLen(core_idx + 1, core_num, len);
 		}
 	}
 	
 	void input() {
-		seq = new ArrayList<>();
 		N = sc.nextInt();
+		max_cnt = 0;
+		min_len = N * N;
 		cell = new int[N][N];
 		core_cnt = 0;
 		
@@ -100,11 +98,10 @@ public class Solution {
 	public Solution() {
 		int T = sc.nextInt();		// number of testcase
 		
-		for (int test_case = 1; test_case <= T; test_case++) {
-//		for (int test_case = 1; test_case <= 1; test_case++) {
+//		for (int test_case = 1; test_case <= T; test_case++) {
+		for (int test_case = 1; test_case <= 1; test_case++) {
 			input();
-			dfs(0);
-			
+			findMinLen(0, 0, 0);
 			System.out.println("#" + test_case + " " + min_len);
 		}
 	}
